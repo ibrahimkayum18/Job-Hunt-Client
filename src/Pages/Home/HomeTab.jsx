4a4323196d18d53a39ea46e36e8463c873125fd6
ibrahimkayum18 <ibrahimkayum017@gmail.com>
2023-11-06 18:26:51 +0600

    Home section filter added

--- src/Pages/Home/HomeTab.jsx
+++ src/Pages/Home/HomeTab.jsx
@@ -5,47 +5,83 @@ import HomeCart from "./HomeCart";
 import { useState } from "react";
 
 const HomeTab = () => {
-  const [filteredData, setFilteredData] = useState([])
   const {data} = UseAllJobs();
+  const [filteredData, setFilteredData] = useState(data)
+  
+  
   const filterItem = (category) => {
-    const findItem = data.filter(item => {
-      item.category === category
-    }); 
     
+    if(category){
+      const findJob = data.filter(item =>  item.category == category );
+      setFilteredData(findJob);
+    }else{
+      setFilteredData(data)
+    }
   }
+  console.log(filteredData);
+    
   return (
     <div>
       <Tabs>
         <TabList>
-          <Tab>All Jobs</Tab>
-          <Tab onClick={() => filterItem('On Site')}>Onsite Jobs</Tab>
-          <Tab onClick={() => filterItem('Remote')}>Remote Jobs</Tab>
-          <Tab onClick={() => filterItem('Hybrid')}>Hybrid Jobs</Tab>
-          <Tab onClick={() => filterItem('Part Time')}>Part Time Jobss</Tab>
+          <Tab onClick={() => filterItem('')}>All Jobs</Tab>
+          <Tab onClick={() => filterItem('on-site')}>Onsite Jobs</Tab>
+          <Tab onClick={() => filterItem('remote')}>Remote Jobs</Tab>
+          <Tab onClick={() => filterItem('hybrid')}>Hybrid Jobs</Tab>
+          <Tab onClick={() => filterItem('part-time')}>Part Time Jobss</Tab>
         </TabList>
 
         <TabPanel style={{
           display:'grid',
           gridTemplateColumns: 'repeat(2,1fr)',
           gap: '20px',
-          padding: '30px 20px'
+          padding: '0 20px'
         }}>
-          {/* {
-            data.map(job => <HomeCart key={job._id} job={job}></HomeCart>)
-          } */}
+          {
+            filteredData.map(job => <HomeCart key={job._id} job={job}></HomeCart>)
+          }
         </TabPanel>
-        <TabPanel>
-          <h2>Any content 2</h2>
+        <TabPanel style={{
+          display:'grid',
+          gridTemplateColumns: 'repeat(2,1fr)',
+          gap: '20px',
+          padding: '0 20px'
+        }}>
+          {
+            filteredData.map(job => <HomeCart key={job._id} job={job}></HomeCart>)
+          }
         </TabPanel>
-        <TabPanel>
-          <h2>Any content 2</h2>
+        <TabPanel style={{
+          display:'grid',
+          gridTemplateColumns: 'repeat(2,1fr)',
+          gap: '20px',
+          padding: '0 20px'
+        }}>
+          {
+            filteredData.map(job => <HomeCart key={job._id} job={job}></HomeCart>)
+          }
         </TabPanel>
-        <TabPanel>
-          <h2>Any content 2</h2>
+        <TabPanel style={{
+          display:'grid',
+          gridTemplateColumns: 'repeat(2,1fr)',
+          gap: '20px',
+          padding: '0 20px'
+        }}>
+          {
+            filteredData.map(job => <HomeCart key={job._id} job={job}></HomeCart>)
+          }
         </TabPanel>
-        <TabPanel>
-          <h2>Any content 2</h2>
+        <TabPanel style={{
+          display:'grid',
+          gridTemplateColumns: 'repeat(2,1fr)',
+          gap: '20px',
+          padding: '0 20px'
+        }}>
+          {
+            filteredData.map(job => <HomeCart key={job._id} job={job}></HomeCart>)
+          }
         </TabPanel>
+        
       </Tabs>
     </div>
   );
